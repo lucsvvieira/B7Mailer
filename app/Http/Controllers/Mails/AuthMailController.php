e6e2c9a34c6eee7b6b6db21873740fe3d381b0c6
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-21 11:16:56 -0300

    Enviando o Primeiro Email

---
+++ app/Http/Controllers/Mails/AuthMailController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Mails;
+
+use App\Http\Controllers\Controller;
+use App\Mail\RegisterEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class AuthMailController extends Controller
+{
+    //
+
+    public function sendRegisterMail() {
+        $registerEmail = new RegisterEmail();
+
+        Mail::to('lucas@email.com')->send($registerEmail);
+    }
+}
